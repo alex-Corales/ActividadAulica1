70aa6ec865f08b736e13813d8d28ba83165c3bba
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-03-21 19:18:13 -0300

    first commit

---
+++ src/main/java/Logica/ActividadAulica1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Logica;
+
+/**
+ *
+ * @author Alex Corales
+ */
+public class ActividadAulica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
