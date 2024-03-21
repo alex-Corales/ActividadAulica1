fe10aa8926f90ab0a782eff945a0fb9d958da02c
Sebastian D'Andrea <sevasdandrea86@gmail.com>
2024-03-21 19:22:57 -0300

    dale javi

--- src/main/java/Logica/ActividadAulica1.java
+++ src/main/java/Logica/ActividadAulica1.java
@@ -11,6 +11,6 @@ package Logica;
 public class ActividadAulica1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Dale javiii!");
     }
 }
