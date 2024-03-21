a1f7b8a5ff929f596b53670a7dd158724083c742
pjfrias <pjfrias@gmail.com>
2024-03-21 19:50:21 -0300

    cambios

---
+++ src/main/java/Logica/Baraja.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author 54265
+ */
+public class Baraja {
+    
+    private arrayList[Carta]; 
+    
+    public void barajar(){
+        
+    } 
+    
+    public void siguienteCarta(){
+        
+    }
+    
+    public void cartasDisponibles(){
+        
+    }
+    
+    public void darCartas(int cantidad){
+        
+    }
+    
+    public void cartasMonton(){
+        
+    }
+    
+    public void mostrarBaraja(){
+        
+    }
+    
+}
