f3393b8916c63ecc01a938d4cf0fb400e956663f
Sebastian D'Andrea <sevasdandrea86@gmail.com>
2024-03-21 20:45:21 -0300

    commit de seba , avisenme cuando suban el array de cartas

--- src/main/java/Logica/Baraja.java
+++ src/main/java/Logica/Baraja.java
@@ -5,36 +5,39 @@
  */
 package Logica;
 
+import java.util.Collections;
+import java.util.HashSet;
+
 /**
  *
  * @author 54265
  */
 public class Baraja {
-    
-    private arrayList[Carta]; 
-    
-    public void barajar(){
-        
-    } 
-    
+
+    private HashSet<Carta>cartas=new HashSet<>(); // este
+
+    public void barajar(HashSet cartas){
+        Collections.shuffle(cartas);
+    }
+
     public void siguienteCarta(){
-        
+
     }
-    
+
     public void cartasDisponibles(){
-        
+
     }
-    
+
     public void darCartas(int cantidad){
-        
+
     }
-    
+
     public void cartasMonton(){
-        
+
     }
-    
+
     public void mostrarBaraja(){
-        
+
     }
-    
+
 }
