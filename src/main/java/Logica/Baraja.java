470ec3656350a52be5438fa1b69fa9d12f49edd9
pjfrias <pjfrias@gmail.com>
2024-03-22 15:32:15 -0300

    monton
    Agregue lista monton

--- src/main/java/Logica/Baraja.java
+++ src/main/java/Logica/Baraja.java
@@ -1,18 +1,21 @@
 package Logica;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Baraja {
 
     private ArrayList<Carta> cartas; // este
-
+    private ArrayList<Carta> monton;
+    
     public Baraja(){
         cartas = new ArrayList<>();
+        monton = new ArrayList<>();
         initBaraja();
     }
     
     private void initBaraja() {
-        String[] palos = {"Espadas", "Bastos", "Oros", "Copas"};
+        String[] palos = {"Espada", "Basto", "Oro", "Copa"};
 
         for (String palo : palos) {
             for (int numero = 1; numero <= 12; numero++) {
@@ -40,10 +43,16 @@ public class Baraja {
     }
 
     public void cartasMonton(){
+        for (Carta carta : monton) {
+            System.out.println(carta);
+        }
 
     }
 
     public void mostrarBaraja(){
+        for (Carta carta : cartas) {
+            System.out.println(carta);
+        }
 
     }
 
